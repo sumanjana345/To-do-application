aa735c9f5d12ffb51dc6ae979d2762be18807f88
sumanjana345 <130403803+sumanjana345@users.noreply.github.com>
2023-04-26 00:08:29 +0530

    Add files via upload

---
+++ to.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+function AddTask(){
+	if(inputBox.value === ''){
+		alert("You must write something here !");
+	}
+	else{
+		let li = document.createElement("li");
+		li.innerHTML = inputBox.value;
+		listcontainer.appendChild(li);
+		let span =document.createElement("span");
+		span.innerHTML="Delete"
+		li.appendChild(span);
+	}
+	inputBox.value= "";
+	savedata();
+}
+
+listcontainer.addEventListener("click",function(e){
+	if(e.target.tagName === "LI"){
+		e.target.classList.toggle("checked");
+		savedata();
+	}
+	else if (e.target.tagName ==="SPAN"){
+		e.target.parentElement.remove();
+		savedata();
+	}
+},false);
+function savedata(){
+	localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showdata(){
+	listcontainer.innerHTML = localStorage.getItem("data");
+}
+showdata();
